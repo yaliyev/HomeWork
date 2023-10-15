2c69de7b0cc6e44ac43cf1832bb296e76ab42e68
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-10-15 17:03:36 +0400

    js tasks part 1

---
+++ 15. Loop Object Algorithms/arrays.js
@@ -0,0 +1,132 @@
+console.log("task1")
+// for(let i = 10; i <= 99;i++){
+//     if(i % 10 === 7){ // % 10 sonuncu reqemi tapmaga imkan verir
+//        console.log(i);  
+//     }
+// }
+console.log("task2");
+// for(let i = 10;i <= 99;i++){
+//     let sonuncuReqem = i % 10;
+//     let sonuncudanEvvelkiReqem  = ((i/10) - ((i/10) % 1))%10; // 10 a boluruk ki son reqem itsin,lakin kesr alinir.Kesride sonradan tam cevirib % 10 la son reqemini tapiriq
+//      if( sonuncuReqem ===  sonuncudanEvvelkiReqem ){ // sonuncudanEvvelkiReqem riyazi usulla tapmisam lakin Math.trunc() mumkun olardi.Calisiram maksimum az elave metod istifade edim))
+//          console.log(i);
+//      }
+// }
+console.log("task3");
+// let ededTask3 = Number(prompt("TASK 3: Eded daxil edin"));
+// let setirCavabTask3 = "";
+
+// for(let i = 0;i <ededTask3.toString().length;i ++){
+//     let setireCevrilmisEded = ededTask3.toString();
+//     setirCavabTask3 += setireCevrilmisEded[i]+" "; 
+// }
+// console.log(setirCavabTask3);
+
+console.log("task4");
+// let ededTask4 = Number(prompt("TASK 4: Eded daxil edin"));
+
+// ededTask4 = Math.trunc(ededTask4);
+
+// let maxTask4 = 0;
+
+// while(ededTask4!=0){
+// if(ededTask4 % 10 > maxTask4 ){
+//     maxTask4 = ededTask4 % 10;
+// }
+//    ededTask4 = Math.trunc( ededTask4 / 10);
+// }
+// console.log(`En boyuk reqem ${maxTask4}`);
+
+console.log("task5");
+
+// let ededTask5 = Number(prompt("TASK 5: Eded daxil edin"));
+
+// let cemTask5 = 0;
+// let hasilTask5 = 1; // 0 yazsaq cavab hemise 0 olar
+// let edediOrtaTask5 = 0;
+// let reqemSayiTask5 = 0;
+
+// while(ededTask5 != 0){
+//     let sonuncuReqemTask5 = ededTask5 % 10;
+//     cemTask5 += sonuncuReqemTask5;
+//     hasilTask5 *= sonuncuReqemTask5;
+//     reqemSayiTask5++;
+
+//     ededTask5 = Math.trunc(ededTask5 / 10);
+
+// }
+// edediOrtaTask5 = cemTask5 / reqemSayiTask5;
+// console.log(cemTask5);
+// console.log(hasilTask5);
+// console.log(edediOrtaTask5);
+
+console.log("task6");
+
+// let ededTask6 = Number(prompt("TASK 6: Eded daxil edin"));
+
+// for(let i = 1; i <= ededTask6;i++){
+//     if(ededTask6 % i === 0){
+//        console.log(i);
+//     }
+//  }
+
+
+ console.log("task7");
+// let ededTask7 = Number(prompt("TASK 7: Eded daxil edin"));
+
+// let bolenSayi = 0;
+
+// for(let i = 1; i <= ededTask7;i++){
+//    if(ededTask7 % i === 0){
+//     bolenSayi++;
+//    }
+// }
+// console.log(bolenSayi);
+
+console.log("task8");
+
+// let arrayTask8 = [1,2,3,4,5,6];
+
+// for(let i = 0;i<arrayTask8.length;i++){
+//     if(arrayTask8[i] % 2 === 1){
+//         console.log("indeks: "+i);
+//     }
+// }
+
+console.log("task9");
+
+// let arrayTask9 = [1,2,3,4,5,6];
+
+// for(let i = 0;i<arrayTask9.length;i++){
+//     if(i % 2 === 1){
+//         console.log(`tek indeksli element: ${arrayTask8[i]}`);
+//     }
+// }
+
+console.log("task10");
+
+// let arrayTask10 = [4,6,10,3,7];
+
+// let maxTask10 = arrayTask10[0];
+
+// for(let i = 0;i<arrayTask10.length;i++){
+//     if(arrayTask10[i]> maxTask10){
+//         maxTask10 = arrayTask10[i];
+//     }
+// }
+// console.log(maxTask10);
+
+console.log();
+
+
+
+
+
+
+
+
+
+
+
+
+
