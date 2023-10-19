439caca900fe243b84bef5fb2647d198ea2d2622
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-10-20 02:02:34 +0400

    9 tasks completed(custom-methods)

--- 19. JS custom methods, callback functions (prototype)/custom-methods.js
+++ 19. JS custom methods, callback functions (prototype)/custom-methods.js
@@ -53,6 +53,79 @@ Array.prototype.numbers = function () {
     return elementCount;
 }
 
+Array.prototype.myFind = function (value) {
+    let exist = false;
+
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === value) {
+            exist = true;
+            break;
+        }
+    }
+    return exist;
+}
+
+Array.prototype.myFindAll = function (value) {
+    let exist = false;
+    let elementCount = -1;
+
+    for (let i = 0; i < this.length; i++) {
+        if (this[i] === value) {
+            if (elementCount === -1) {
+                elementCount = 0;
+                exist = true;
+            }
+            elementCount++;
+        }
+    }
+    return elementCount;
+}
+
+Array.prototype.myFilter = function (min, max) {
+    let newArr = [];
+    let count = 0;
+
+    if (max > min) { // elave kod(mentiqi olaraq bele olmalidir)
+
+        // elave metodlar qadagan olduguna gore,push istifade etmek evezine count vasitesile indeksleme usulu ile massivi dolduracayiq
+        for (let i = min; i < max; i++) {
+            newArr[count] = this[i];
+            count++;
+        }
+
+    }
+
+
+
+    return newArr;
+
+}
+
+Array.prototype.myIndexOf = function (value){
+    let index = -1;
+
+    for(let i = 0; i < this.length;i++){
+        if(this[i] === value ){
+            index = i;
+            break; // niyede axra kimi davam elesin? Elnur muellim oyredib bu usulu(zamana gore tez bitir for dovru).Best Case prinsiplerinden biridir.
+        }
+    }
+    return index;
+} 
+
+Array.prototype.myLastIndexOf = function (value){
+    let index = -1;
+
+    for(let i = this.length - 1; i >= 0;i--){
+        if(this[i] === value ){
+            index = i;
+            break; 
+        }
+    }
+    return index;
+}
+
+
 // testing methods
 
 console.log('----task1----');
@@ -64,6 +137,7 @@ console.log('----task2----');
 console.log('salam'.wavy());
 
 let numArr = [10, 5, 6, 7, 2, 20];
+
 let arr = [1, "salam", 2, 7, true, 12, false];
 
 console.log('----task3----');
@@ -71,3 +145,13 @@ console.log(numArr.min());
 console.log(numArr.max());
 console.log('----task4----');
 console.log(arr.numbers());
+console.log('----task5----');
+console.log(arr.myFind("salam"));
+console.log('----task6----');
+console.log([5, 10, 20, 10].myFindAll(10));
+console.log('----task7----');
+console.log([10, 20, 30, 40, 50].myFilter(1, 3)); // burada 2 element qayidacaq,taskin shertlerinde arr.myFilter(3,8) -> 4 elementli massiv qaytarirdi.Bu sebebden min elementi daxildir,max daxil deyil(max qeder nezere alinir)
+console.log('----task8----');
+console.log([10,20,30,40].myIndexOf(20));
+console.log('----task9----');
+console.log([10,20,30,20,30,40].myLastIndexOf(20));
