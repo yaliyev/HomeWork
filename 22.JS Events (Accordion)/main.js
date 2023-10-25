d76b708676e5748ebb577c1acc23c4831fd9138b
yaliyev <tu6w6s3r7@code.edu.az>
2023-10-25 21:47:57 +0400

    code changes successfully added

--- 22.JS Events (Accordion)/main.js
+++ 22.JS Events (Accordion)/main.js
@@ -5,17 +5,25 @@ items.forEach(item => {
     const according = item.querySelector('.according');
     const icon = item.querySelector('.icon');
 
+    according.style.display = 'none';
+
     head.addEventListener('click', () => {
         if (according.style.display === 'block' || according.style.display === '') {
             according.style.display = 'none';
             icon.innerHTML = '&#9660;';
+            head.classList.remove('active');
         } else {
             items.forEach(otherItem => {
                 const otherContent = otherItem.querySelector('.according');
                 otherContent.style.display = 'none';
+                const otherIcon = otherItem.querySelector('.icon');
+                otherIcon.innerHTML = '&#9660;';
+                const otherHead = otherItem.querySelector('.header');
+                otherHead.classList.remove('active');
             });
             according.style.display = 'block';
             icon.innerHTML = '&#9650;';
+            head.classList.add('active'); // aktivləşdikdə "active" klassını əlavə et
         }
     });
 });
