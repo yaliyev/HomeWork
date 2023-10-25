561c94619a546dbcc0c89fe558db7b7cb783df41
yaliyev <tu6w6s3r7@code.edu.az>
2023-10-25 21:26:14 +0400

    arrows are completed

--- 22.JS Events (Accordion)/main.js
+++ 22.JS Events (Accordion)/main.js
@@ -3,16 +3,19 @@ const items = document.querySelectorAll('.item');
 items.forEach(item => {
     const head = item.querySelector('.header');
     const according = item.querySelector('.according');
+    const icon = item.querySelector('.icon');
 
     head.addEventListener('click', () => {
         if (according.style.display === 'block' || according.style.display === '') {
             according.style.display = 'none';
+            icon.innerHTML = '&#9660;';
         } else {
             items.forEach(otherItem => {
                 const otherContent = otherItem.querySelector('.according');
                 otherContent.style.display = 'none';
             });
             according.style.display = 'block';
+            icon.innerHTML = '&#9650;';
         }
     });
 });
