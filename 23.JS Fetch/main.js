667cdf84224e40190329e4325035b119e7b6a884
yaliyev <tu6w6s3r7@code.edu.az>
2023-10-27 00:40:24 +0400

    buttons added, functionality completed

--- 23.JS Fetch/main.js
+++ 23.JS Fetch/main.js
@@ -1,32 +1,101 @@
-const api = 'https://jsonplaceholder.typicode.com/users';
+let api = "https://jsonplaceholder.typicode.com/users";
+let userData = [];
 
-      function fetchTable() {
-        fetch(api)
-          .then((response) => response.json())
-          .then((data) => {
-            const tableBody = document.getElementById('table-body');
+function fetchTable() {
+  fetch(api)
+    .then((response) => response.json())
+    .then((data) => {
+      userData = data;
+      let tableBody = document.getElementById("table-body");
+      tableBody.innerHTML = "";
 
-            data.forEach((user, index) => {
-              const row = `
-                <tr>
-                  <th scope="row">${index + 1}</th>
-                  <td>${user.name}</td>
-                  <td>${user.username}</td>
-                  <td>${user.email}</td>
-                  <td>${user.address.city}</td>
-                  <td><a href="${user.website}" target="_blank">${user.website}</a></td>
-                  <td>
-                    <button class="btn btn-primary" onclick="showInfo(${user.id})">Show Info</button>
-                  </td>
-                </tr>
-              `;
-              tableBody.insertAdjacentHTML('beforeend', row);  // bunun vasitesile datani herdefe table a doldururuq
-            });
-          });
-      }
+      data.forEach((user, index) => {
+        let row = `
+          <tr>
+            <th scope="row">${user.id}</th>
+            <td>${user.name}</td>
+            <td>${user.username}</td>
+            <td>${user.email}</td>
+            <td>${user.address.city}</td>
+            <td><a href="${user.website}" target="_blank">${user.website}</a></td>
+            <td>
+              <button class="btn btn-info" onclick="showInfo(${user.id})">Show Info</button>
+            </td>
+          </tr>
+        `;
+        tableBody.insertAdjacentHTML("beforeend", row);
+      });
+    });
+}
 
-      window.addEventListener('load', fetchTable);
+function showInfo(userId) {
+}
 
-      function showInfo(userId) {
-        
-      }
+function updateTable() {
+  let tableBody = document.getElementById("table-body");
+  tableBody.innerHTML = "";
+
+  userData.forEach((user, index) => {
+    let row = `
+      <tr>
+        <th scope="row">${user.id}</th>
+        <td>${user.name}</td>
+        <td>${user.username}</td>
+        <td>${user.email}</td>
+        <td>${user.address.city}</td>
+        <td><a href="${user.website}" target="_blank">${user.website}</a></td>
+        <td>
+          <button class="btn btn-info" onclick="showInfo(${user.id})">Show Info</button>
+        </td>
+      </tr>
+    `;
+    tableBody.insertAdjacentHTML("beforeend", row);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let getDataButton = document.getElementById("getDataButton");
+  let sortByUsernameButton = document.getElementById("sortByUsernameButton");
+  let sortByIdButton = document.getElementById("sortByIdButton");
+  let clearTableButton = document.getElementById("clearTableButton");
+
+  function sortByUsername() {
+    userData.sort((a, b) => {
+      return a.username.localeCompare(b.username);
+    });
+
+    updateTable();
+  }
+
+  function sortById() {
+    userData.sort((a, b) => b.id - a.id);
+    updateTable();
+  }
+
+  function clearTable() {
+    userData = [];
+    let tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = "";
+    getDataButton.disabled = false;
+  }
+
+  sortByUsernameButton.addEventListener("click", function () {
+    sortByUsername();
+  });
+
+  sortByIdButton.addEventListener("click", function () {
+    sortById();
+  });
+
+  clearTableButton.addEventListener("click", function () {
+    clearTable();
+  });
+
+  getDataButton.addEventListener("click", function () {
+    fetchTable();
+    getDataButton.disabled = true;
+  });
+});
+
+
+
