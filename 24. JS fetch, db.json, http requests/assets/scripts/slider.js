0bfbf978ea2c8eb11704859c19c1ba67cbf1829f
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-10-30 03:02:49 +0400

    slider completed

---
+++ 24. JS fetch, db.json, http requests/assets/scripts/slider.js
@@ -0,0 +1,54 @@
+let images = ["../assets/images/breaking-bad-1.jpg","../assets/images/breaking-bad-2.jpg","../assets/images/breaking-bad-3.jpg","../assets/images/breaking-bad-4.jpeg"];
+
+let currentImage = 0;
+
+let sliderImage = document.getElementById('slider-image');
+
+let sliderControlLeftButton = document.getElementById('slider-control-button-left');
+
+let sliderControlRightButton = document.getElementById('slider-control-button-right');
+
+sliderControlLeftButton.addEventListener('click',function(){
+    if(currentImage >= 1){
+       
+       document.getElementById('slider').style.animationName = 'slider-image-movement-right';
+       delayPreviousImageAnimation();
+    }
+});
+
+sliderControlRightButton.addEventListener('click',function(){
+    if(currentImage < images.length - 1){
+        document.getElementById('slider').style.animationName = 'slider-image-movement-left';
+        delayNextImageAnimation();
+    }
+ });
+
+function previousImage(){
+
+    currentImage--;
+    
+    sliderImage.src = images[currentImage];
+}
+
+function nextImage(){
+
+    currentImage++;
+    
+    sliderImage.src = images[currentImage];
+}
+
+async function delayPreviousImageAnimation(){
+    setTimeout(()=>{
+        previousImage();
+        document.getElementById('slider').style.animationName = 'slider-image-movement-from-left-to-right';
+       },1500);
+       
+}
+
+async function delayNextImageAnimation(){
+    setTimeout(()=>{
+        nextImage();
+        document.getElementById('slider').style.animationName = 'slider-image-movement-from-right-to-left';
+       },1500);
+       
+}
