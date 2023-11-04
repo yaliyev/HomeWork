a88e58674a3f686312534e825a4f01215a89b3da
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-11-04 04:20:46 +0400

    user register layout and regex

--- 25. Storage task/front-end/assets/scripts/header.js
+++ 25. Storage task/front-end/assets/scripts/header.js
@@ -1,11 +1,17 @@
-
+import { getUsersData } from './requests/usersRequests.js';
 let favouritesStr = localStorage.getItem("favorites");
+
+let users = [];
+
+(async () => {
+  users = await getUsersData();
+})();
 
 
 let favourites = [];
 
 if (favouritesStr != null) {
-    favourites = JSON.parse(favouritesStr);
+  favourites = JSON.parse(favouritesStr);
 }
 
 let basketStr = localStorage.getItem("basket");
@@ -14,7 +20,7 @@ let basketStr = localStorage.getItem("basket");
 let basket = [];
 
 if (basketStr != null) {
-    basket = JSON.parse(basketStr);
+  basket = JSON.parse(basketStr);
 }
 
 
@@ -22,32 +28,174 @@ let wishListCountElement = document.getElementById('wishlist-count');
 
 let basketCountElement = document.getElementById('basket-count');
 
-document.querySelector(".nav-logo-box").addEventListener('click',function(){
+let loginButton = document.getElementById('user-login-button');
+
+let registerButton = document.getElementById('user-register-button');
+
+
+
+document.querySelector(".nav-logo-box").addEventListener('click', function () {
   window.location.href = "/front-end/templates/main.html";
 });
 
-function countWishListElements(){
-  
+
+
+
+registerButton.addEventListener('click', function () {
+  Swal.fire({
+    title: '<strong>User Register</strong>',
+    html: `
+    <div class="form-group">
+    <label style="width:100%;text-align:left;margin-top:10px;margin-bottom:10px;"><b>Username</b> </label>
+    <input  type="text" class="form-control" id="register-username" placeholder="Enter Username">
+  <div id='username-validation-result' class='my-2 text-danger'></div>
+  </div>
+  <div class="form-group">
+  <label style="width:100%;text-align:left;margin-top:10px;margin-bottom:10px;"><b>Email</b> </label>
+  <input type="email" class="form-control" id="register-email" placeholder="Enter Email">
+  <div id='email-validation-result' class='my-2 text-danger'></div>
+</div>
+<div class="form-group">
+<label style="width:100%;text-align:left;margin-top:10px;margin-bottom:10px;"><b>Password</b> </label>
+<input type="password" class="form-control" id="register-password" placeholder="Enter Password">
+<div id='password-validation-result' class='my-2 text-danger'></div>
+</div>
+<div class="form-group">
+<label style="width:100%;text-align:left;margin-top:10px;margin-bottom:10px;"><b>Confirm Password</b> </label>
+<input type="password" class="form-control" id="register-confirm-password" placeholder="Enter Password">
+<div id='confirm-password-validation-result' class='my-2 text-danger'></div>
+</div>
+<div class="form-group">
+  <label style="width:100%;text-align:left;margin-top:10px;margin-bottom:10px;"><b>Balance</b> </label>
+  <input type="number" class="form-control" id="register-balance" placeholder="Enter Balance">
+  <div id='balance-validation-result' class='text-danger'></div>
+</div>
+
+  `,
+    focusConfirm: false,
+    confirmButtonText:
+      'Register',
+    showCancelButton: true,
+    cancelButtonText:
+    'Close'
+  }).then((result) => {
+
+    if (result.isConfirmed) {
+
+    }
+
+  });
+  let container = Swal.getHtmlContainer();
+
+  container.querySelector('#register-username').addEventListener('keyup', function () {
+    let user = users.find((element) => {
+      if (element.username.toLowerCase() === this.value.toLowerCase()) {
+        document.querySelector('#username-validation-result').innerText = 'username is already exists';
+        return element;
+      }
+    });
+
+
+
+
+
+    if (user == undefined) {
+      document.querySelector('#username-validation-result').innerText = '';
+      if (this.value.match('^[A-Za-z0-9]{3,}$') == null) {
+        document.querySelector('#username-validation-result').innerText = 'username must be minimum 3 characters';
+      }
+      if (this.value.trim().length == 0) {
+        document.querySelector('#username-validation-result').innerText = 'username is required';
+      }
+
+    }
+
+  });
+
+  container.querySelector('#register-email').addEventListener('keyup', function () {
+    let user = users.find((element) => {
+      if (element.email.toLowerCase() === this.value.toLowerCase()) {
+        document.querySelector('#email-validation-result').innerText = 'email is already exists';
+        return element;
+      }
+    });
+
+    if (user == undefined) {
+      document.querySelector('#email-validation-result').innerText = '';
+      if (this.value.match('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$') == null) {
+        document.querySelector('#email-validation-result').innerText = 'email format is incorrect';
+      }
+
+    }
+
+  });
+
+  container.querySelector('#register-password').addEventListener('keyup', function () {
+
+    document.querySelector('#password-validation-result').innerText = '';
+    if (this.value.match('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{5,}') == null) {
+      document.querySelector('#password-validation-result').innerText = 'password must be minimum 5 characters[aA9]';
+    }
+    if (this.value.trim().length == 0) {
+      document.querySelector('#password-validation-result').innerText = 'password is required';
+    }
+
+
+
+
+  });
+
+  container.querySelector('#register-confirm-password').addEventListener('keyup', function () {
+
+    document.querySelector('#confirm-password-validation-result').innerText = '';
+    if (this.value != container.querySelector('#register-password').value) {
+      document.querySelector('#confirm-password-validation-result').innerText = 'confirm password & password are not the same';
+    }
+    if (this.value.trim().length == 0) {
+      document.querySelector('#confirm-password-validation-result').innerText = 'confirm password is required';
+    }
+  });
+
+  container.querySelector('#register-balance').addEventListener('keyup', function () {
+
+    document.querySelector('#balance-validation-result').innerText = '';
+    if (Number(this.value) < 0) {
+      document.querySelector('#balance-validation-result').innerText = 'balance must be positive';
+    }
+    if(this.value.trim().length == 0) {
+      document.querySelector('#balance-validation-result').innerText = 'balance is required';
+    }
+  });
+
+});
+
+
+
+
+function countWishListElements() {
+
   wishListCountElement.innerText = favourites.length;
 }
 
-function countBasketElements(){
-  
+function countBasketElements() {
+
   basketCountElement.innerText = basket.length;
 }
 
 countWishListElements();
 countBasketElements();
 
-export  function increaseWishListElementsCount(){
-  wishListCountElement.innerText = Number(wishListCountElement.innerText)+1;
+
+
+export function increaseWishListElementsCount() {
+  wishListCountElement.innerText = Number(wishListCountElement.innerText) + 1;
 }
-export  function decreaseWishListElementsCount(){
+export function decreaseWishListElementsCount() {
   wishListCountElement.innerText = Number(wishListCountElement.innerText) - 1;
 }
-export  function increaseBasketElementsCount(){
-  basketCountElement.innerText = Number(basketCountElement.innerText)+1;
+export function increaseBasketElementsCount() {
+  basketCountElement.innerText = Number(basketCountElement.innerText) + 1;
 }
-export  function decreaseBasketElementsCount(){
+export function decreaseBasketElementsCount() {
   basketCountElement.innerText = Number(basketCountElement.innerText) - 1;
 }
