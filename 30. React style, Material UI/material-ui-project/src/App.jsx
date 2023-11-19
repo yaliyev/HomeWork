5636649bb7243f1f795b83c9f02c3427d4590ad2
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-11-20 02:20:18 +0400

    material ui project config

---
+++ 30. React style, Material UI/material-ui-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Button from '@mui/material/Button';
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
