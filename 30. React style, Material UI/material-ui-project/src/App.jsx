25063a6c167f4f5374f8eb709471b23505221c03
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-11-20 02:48:06 +0400

    responsive navbar

--- 30. React style, Material UI/material-ui-project/src/App.jsx
+++ 30. React style, Material UI/material-ui-project/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import Button from '@mui/material/Button';
 import './App.css'
+import Navbar from './components/navbar';
 
 function App() {
 
   return (
     <>
-    
+     <Navbar/>
     </>
   )
 }
---
+++ 30. React style, Material UI/material-ui-project/src/components/navbar.jsx
@@ -0,0 +1,64 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import MenuIcon from '@mui/icons-material/Menu';
+import Container from '@mui/material/Container';
+
+
+
+
+const pages = ['Products', 'Pricing', 'Blog'];
+const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
+
+function Navbar() {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <MenuIcon sx={{ display: { xs: 'flex', md: 'flex' }, mr: 1 }} />
+          <Typography
+            variant="h6"
+            noWrap
+            component="a"
+            href="#app-bar-with-responsive-menu"
+            sx={{
+              mr: 2,
+              display: { xs: 'flex', md: 'flex' },
+              fontFamily: 'sans-serif',
+              fontWeight: 400,
+              color: 'inherit',
+              textDecoration: 'none',
+            }}
+          >
+            Album layout
+          </Typography>
+
+          
+        
+         
+
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
+export default Navbar;
