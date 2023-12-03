6aa2cf724cdd111f77af7282ef0e36056c6e959c
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-12-04 01:29:37 +0400

    register has been completed

--- 32. Redux Toolkit Project/redux-toolkit-react-project-yagub/src/routes/routes.jsx
+++ 32. Redux Toolkit Project/redux-toolkit-react-project-yagub/src/routes/routes.jsx
@@ -1,9 +1,25 @@
-import UserRoot from '../pages/user/UserRoot'
+import LoginPage from '../pages/LoginPage';
+import RegisterPage from '../pages/RegisterPage'
+import UserRoot from '../pages/UserRoot'
 
 
 export const ROUTES = [
     {
         path:'/',
-        element: <UserRoot/>
+        element: <UserRoot/>,
+        children:[
+            {
+                index: true,
+                element:<RegisterPage/>
+            },
+            {
+                path:'/registerPage',
+                element:<RegisterPage/>
+            },
+            {
+                path:'/loginPage',
+                element:<LoginPage/>
+            }
+        ]
     }
 ];
