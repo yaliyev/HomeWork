de0a6f6f8d546b1634a1af2a6a4235912d4d4bb8
YaqubAliyev4513 <tu6w6s3r7@code.edu.az>
2023-12-15 02:29:17 +0400

    task has been completed

--- 43. TypeScript Day 2( OOP Task)/main.js
+++ 43. TypeScript Day 2( OOP Task)/main.js
@@ -11,3 +11,98 @@ var EngineType;
     EngineType[EngineType["TURBO"] = 2] = "TURBO";
 })(EngineType || (EngineType = {}));
 let Capacity;
+class Car {
+    constructor(brandname, modelname, year, fuelCapacity, currentFuel, milage, fuelFor1KM, engine) {
+        this.vehicleType = VehicleType.Car; // onsuzda hemishe car dusecek,default deyerdi
+        this.brandName = brandname;
+        this._modelName = modelname;
+        this._year = year;
+        this._fuelCapacity = fuelCapacity;
+        this._currentFuel = currentFuel;
+        this._milage = milage;
+        this._fuelFor1KM = fuelFor1KM;
+        this.engine = engine;
+    }
+    get modelName() {
+        return this._modelName;
+    }
+    get year() {
+        return this._year;
+    }
+    get fuelCapacity() {
+        return this._fuelCapacity;
+    }
+    get currentFuel() {
+        return this._currentFuel;
+    }
+    get milage() {
+        return this._milage;
+    }
+    get fuelFor1KM() {
+        return this._fuelFor1KM;
+    }
+    getInfo() {
+        console.log(this.brandName);
+        console.log(this._modelName);
+        console.log(this._year);
+        console.log(this._fuelCapacity);
+        console.log(this._currentFuel);
+    }
+    drive(km) {
+        this._milage += km;
+        this._currentFuel -= km;
+        return this._currentFuel;
+    }
+}
+class MotorCycle {
+    constructor(brandname, modelname, year, fuelCapacity, currentFuel, milage, fuelFor1KM, engine) {
+        this.vehicleType = VehicleType.MotorCycle;
+        this.brandName = brandname;
+        this._modelName = modelname;
+        this._year = year;
+        this._fuelCapacity = fuelCapacity;
+        this._currentFuel = currentFuel;
+        this._milage = milage;
+        this._fuelFor1KM = fuelFor1KM;
+        this.engine = engine;
+    }
+    get modelName() {
+        return this._modelName;
+    }
+    get year() {
+        return this._year;
+    }
+    get fuelCapacity() {
+        return this._fuelCapacity;
+    }
+    get currentFuel() {
+        return this._currentFuel;
+    }
+    get milage() {
+        return this._milage;
+    }
+    get fuelFor1KM() {
+        return this._fuelFor1KM;
+    }
+    getInfo() {
+        console.log(this.brandName);
+        console.log(this._modelName);
+        console.log(this._year);
+        console.log(this._fuelCapacity);
+        console.log(this._currentFuel);
+    }
+    drive(km) {
+        this._milage += km;
+        this._currentFuel -= km;
+        return this._currentFuel;
+    }
+    startEngine() {
+        console.log(`Honda CBR600R's engine is now running`);
+    }
+}
+let car = new Car('BMW', "M5", 2019, 100, 60, 10000, 10, EngineType.DOHC);
+let motor = new MotorCycle('Harley Davidson', 'LV9', 2008, 70, 50, 5000, 6, EngineType.SOHC);
+let carHeadingElement = document.querySelector("#car-name");
+let motorHeadingElement = document.querySelector("#motor-name");
+carHeadingElement.textContent = car.brandName + ' ' + car.modelName;
+motorHeadingElement.textContent = motor.brandName + ' ' + motor.modelName;
--- 43. TypeScript Day 2( OOP Task)/main.ts
+++ 43. TypeScript Day 2( OOP Task)/main.ts
@@ -9,17 +9,174 @@ enum EngineType{
     TURBO
 }
 
-let Capacity : '30' | '40' | '50' | '70' | '100';
+let Capacity : 30 | 40 | 50 | 70 | 100;
 
 interface IVehicle<T>{
-   brandName:string,
-   modelName:string,
-   year:number,
-   fuelCapacity:number,
-   currentFuel: T,
-   getInfo:()=>void,
+   brandName:string;
+   modelName:string;
+   year:number;
+   fuelCapacity:number;
+   currentFuel: T;
+   milage:number;
+   fuelFor1KM:number;
+   engine: EngineType;
+   vehicleType: VehicleType;
+
+   getInfo:()=>void;
    drive:(km:number)=>T
 }
+
+class Car implements IVehicle<number>{ 
+    public brandName: string;
+    private _modelName: string;
+    private _year: number;
+    private _fuelCapacity: number;
+    private _currentFuel:number;
+    private _milage:number;
+    private _fuelFor1KM:number;
+    
+    vehicleType: VehicleType = VehicleType.Car; // onsuzda hemishe car dusecek,default deyerdi
+
+    engine: EngineType;
+    
+    constructor(brandname:string,modelname:string,year:number,fuelCapacity:number,currentFuel:number,milage:number,fuelFor1KM:number,engine:EngineType){
+        this.brandName = brandname;
+        this._modelName = modelname;
+        this._year = year;
+        this._fuelCapacity = fuelCapacity;
+        this._currentFuel = currentFuel;
+        this._milage = milage;
+        this._fuelFor1KM = fuelFor1KM;
+        this.engine = engine;
+    }
+
+    get modelName() {
+     return this._modelName;
+    }
+    
+    get year(){
+        return this._year;
+    }
+
+    get fuelCapacity(){
+        return this._fuelCapacity;
+    }
+
+    get currentFuel(){
+        return this._currentFuel;
+    }
+
+    get milage(){
+        return this._milage;
+    }
+
+    get fuelFor1KM(){
+        return this._fuelFor1KM;
+    }
+    
+    getInfo():void{
+      console.log(this.brandName);
+      console.log(this._modelName);
+      console.log(this._year);
+      console.log(this._fuelCapacity);
+      console.log(this._currentFuel);
+    }
+
+    drive(km: number): number{
+        this._milage += km;
+        this._currentFuel -= km;
+
+        return this._currentFuel;
+    }
+
+    
+}
+
+class MotorCycle implements IVehicle<number>{
+    public brandName: string;
+    private _modelName: string;
+    private _year: number;
+    private _fuelCapacity: number;
+    private _currentFuel:number;
+    private _milage:number;
+    private _fuelFor1KM:number;
+
+    vehicleType: VehicleType = VehicleType.MotorCycle;
+
+    engine: EngineType;
+    
+    constructor(brandname:string,modelname:string,year:number,fuelCapacity:number,currentFuel:number,milage:number,fuelFor1KM:number,engine:EngineType){
+        this.brandName = brandname;
+        this._modelName = modelname;
+        this._year = year;
+        this._fuelCapacity = fuelCapacity;
+        this._currentFuel = currentFuel;
+        this._milage = milage;
+        this._fuelFor1KM = fuelFor1KM;
+        this.engine = engine;
+    }
+
+    get modelName() {
+     return this._modelName;
+    }
+    
+    get year(){
+        return this._year;
+    }
+
+    get fuelCapacity(){
+        return this._fuelCapacity;
+    }
+
+    get currentFuel(){
+        return this._currentFuel;
+    }
+
+    get milage(){
+        return this._milage;
+    }
+
+    get fuelFor1KM(){
+        return this._fuelFor1KM;
+    }
+    
+    getInfo():void{
+      console.log(this.brandName);
+      console.log(this._modelName);
+      console.log(this._year);
+      console.log(this._fuelCapacity);
+      console.log(this._currentFuel);
+    }
+
+    drive(km: number): number{
+        this._milage += km;
+        this._currentFuel -= km;
+
+        return this._currentFuel;
+    }
+
+    startEngine():void{
+        console.log(`Honda CBR600R's engine is now running`);
+    }
+}
+
+let car = new Car('BMW',"M5",2019,100,60,10000,10,EngineType.DOHC);
+
+let motor = new MotorCycle('Harley Davidson','LV9',2008,70,50,5000,6,EngineType.SOHC);
+
+let carHeadingElement = document.querySelector<HTMLHeadingElement>("#car-name")!;
+
+let motorHeadingElement = document.querySelector<HTMLHeadingElement>("#motor-name")!;
+
+
+
+
+
+carHeadingElement.textContent = car.brandName + ' ' + car.modelName;
+
+motorHeadingElement.textContent = motor.brandName + ' ' + motor.modelName;
+
+
 
 
 
